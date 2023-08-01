3479f1f462eee37689f01221af0aca21e7d2a1a0
NazarovPV <nazar969@yandex.ru>
2023-08-01 09:40:59 +0300

    style
    Изменить табуляцию, отступы, знаки препинания.

--- src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
@@ -1,14 +1,12 @@
 package ru.yandex.practicum.filmorate.controllers;
 
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import javax.validation.Valid;
-
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -38,12 +36,12 @@ public class FilmController {
             throw new ValidationException("Введен неверный год выпуска");
         }
         id++;
-        films.put(newFilm.getId(),newFilm);
-        return newFilm ;
+        films.put(newFilm.getId(), newFilm);
+        return newFilm;
     }
 
     @PutMapping("/films")
-    public Film updateFilm(@RequestBody @Valid Film updatedFilm){
+    public Film updateFilm(@RequestBody @Valid Film updatedFilm) {
         if (!films.containsKey(updatedFilm.getId())) {
             log.info("Запрос на обновление фильма не выполнен из-за id");
             throw new ValidationException("Введен неверный id");
@@ -51,7 +49,7 @@ public class FilmController {
         log.info("Запрос на обновление фильма выполнен");
 
         films.remove(updatedFilm.getId());
-        films.put(updatedFilm.getId(),updatedFilm);
+        films.put(updatedFilm.getId(), updatedFilm);
         return updatedFilm;
     }
 
