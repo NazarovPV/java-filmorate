57475d881ddb17ba22c97525f1e2d359b4a54716
NazarovPV <nazar969@yandex.ru>
2023-10-04 11:12:52 +0300

    feat
    Изменить кодировку

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,9 +4,9 @@ MERGE INTO MPA (id,name) VALUES (3,'PG-13');
 MERGE INTO MPA (id,name) VALUES (4,'R');
 MERGE INTO MPA (id,name) VALUES (5,'NC-17');
 
-MERGE INTO GENRE (id,name) VALUES (1,'Комедия');
-MERGE INTO GENRE (id,name) VALUES (2,'Драма');
-MERGE INTO GENRE (id,name) VALUES (3,'Мультфильм');
-MERGE INTO GENRE (id,name) VALUES (4,'Триллер');
-MERGE INTO GENRE (id,name) VALUES (5,'Документальный');
-MERGE INTO GENRE (id,name) VALUES (6,'Боевик');
+MERGE INTO GENRE (id,name) VALUES (1,'РљРѕРјРµРґРёСЏ');
+MERGE INTO GENRE (id,name) VALUES (2,'Р”СЂР°РјР°');
+MERGE INTO GENRE (id,name) VALUES (3,'РњСѓР»СЊС‚С„РёР»СЊРј');
+MERGE INTO GENRE (id,name) VALUES (4,'РўСЂРёР»Р»РµСЂ');
+MERGE INTO GENRE (id,name) VALUES (5,'Р”РѕРєСѓРјРµРЅС‚Р°Р»СЊРЅС‹Р№');
+MERGE INTO GENRE (id,name) VALUES (6,'Р‘РѕРµРІРёРє');
